73a3d6b6bb56a61850204da9ecd969dd55a9c79b
fawazmez <fawaz.mezher@ocsbstudent.ca>
2018-02-12 14:50:02 -0500

    int as well as image and name
    name and image on the screen

--- main.lua
+++ main.lua
@@ -7,5 +7,15 @@
 --
 --this program saids hello world with text and sprit
 -----------------------------------------------------------------------------------------
-
--- 
+-- show the text fawaz mezher
+ local myText = display.newText( "Fawaz Mezher", 768, 500, native.systemFont, 200 )
+-- text color
+myText:setFillColor( 1, 0, 0 )
+-- makes background white
+display.setDefault( "background", 36, 28, 1 )
+--the sprit
+local image = display.newImageRect( "assist/sprits/Idle (1).png",1000,1000 )
+-- cenetering it
+image.X = 3000
+image.y = 1000
+image:translate(800, 100)
